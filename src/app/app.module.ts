b0ad2b91276d0397d38dbe5081c37cc29cf5cb20
LauraM <lauramarcela089@gmail.com>
2018-08-03 20:49:16 -0500

    new change
    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,37 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Import firebase Angular 
+import { AngularFireModule } from "angularfire2"
+import { AngularFireAuthModule } from "angularfire2/auth"
+
 import { MyApp } from './app.component';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 import { HomePage } from '../pages/home/home';
+import { PreguntaPage } from '../pages/pregunta/pregunta';
+import { FinPage } from '../pages/fin/fin';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PreguntaPage,
+    FinPage
+    
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PreguntaPage,
+    FinPage
   ],
   providers: [
     StatusBar,
